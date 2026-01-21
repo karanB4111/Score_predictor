76255ef23758aafc7508e5e4effb6e4a4f92eafc
karanB4111 <karanbhadarka37@gmail.com>
2026-01-21 21:01:30 +0530

    Template.py and making folders

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'ML_Project',
+    version = '0.0.1',
+    author='Kanu Bhadaraka',
+    author_email='karanbhadarka37@gmail.com',
+    packages = find_packages(),
+)
