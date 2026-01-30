0ec09c2b7b41796dfda03060ea7dc672c78a87bc
karanB4111 <karanbhadarka37@gmail.com>
2026-01-31 01:12:47 +0530

    Model trainer

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -4,6 +4,8 @@ import pandas as pd
 import sys
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 import os
 
 def save_object(file_path, obj):
@@ -14,6 +16,36 @@ def save_object(file_path, obj):
 
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test,models,param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            X_train_score = r2_score(y_train,y_train_pred)
+
+            X_test_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = X_test_score
+
+        return report
+
 
     except Exception as e:
         raise CustomException(e,sys)
